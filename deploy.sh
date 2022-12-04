971a68d87348d51c94ac8d3249de8d9601df5f3e
Jailson Anjos <jailsonanjos@hotmail.com>
2022-12-04 12:08:02 -0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -21,9 +21,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:jairocket/jairocket.github.io.git main
+# git push -f git@github.com:jairocket/jairocket.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+# git push -f git@github.com:jairocket/jailsonanjos.git main:gh-pages
 
 cd -
